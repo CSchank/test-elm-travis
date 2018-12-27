f8c41ced6aae892b9321cca9589d8482a9b93f20
CSchank <chris@schankula.ca>
2018-12-27 17:18:55 -0500

    run elm-format

--- src/Main.elm
+++ src/Main.elm
@@ -23,8 +23,7 @@ update : Msg -> Model -> Model
 update msg model =
     case msg of
         Increment ->
-            { model | count = model.count + 1 
-            }
+            { model | count = model.count + 1 }
 
         Decrement ->
             { model | count = model.count - 1 }
